149ccce8a8e79ea7234795eb3c90adbd564aa058
Jose Luis <joseluislavado@gmail.com>
2019-03-08 17:31:36 +0100

    Some fixes and add docker compose template to stand up microservices

--- src/main/kotlin/cli/Update.kt
+++ src/main/kotlin/cli/Update.kt
@@ -10,7 +10,7 @@ import java.io.File
 
 class Update :
     CliktCommand(help = "Update all the repos with development") {
-    private val infoFile by option("-i", "--infoFile").file(exists = true).default(File("info.json"))
+    private val infoFile by option("-i", "--infoFile").file(exists = true).default(File("../info.json"))
     private val workspace by option("-w", "--workspace").file(exists = true)
 
     override fun run() {
