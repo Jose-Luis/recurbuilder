6eafbd36d750358cd13454a0e8e6ebcb7884c4db
Jose Luis <joseluislavado@gmail.com>
2019-04-30 15:45:05 +0200

    Add help

--- src/main/kotlin/cli/SetDependencyVersion.kt
+++ src/main/kotlin/cli/SetDependencyVersion.kt
@@ -4,23 +4,21 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.switch
-import com.github.ajalt.clikt.parameters.types.choice
 import com.github.ajalt.clikt.parameters.types.file
-import commands.Builder
 import commands.DependencyVersioner
-import commands.Updater
-import commands.Versioner
 import info.Info
 import java.io.File
 
 class SetDependencyVersion() :
     CliktCommand(
-        help = "Set external dependency version",
-        name = "dep-version" ) {
-    private val artifactDescriptor by argument().multiple()
+        help = "Set external dependency version ",
+        name = "dep-version"
+    ) {
+    private val artifactDescriptor by argument(
+        name = "artifact-descriptor",
+        help = "groupId:artifactId:version"
+    ).multiple()
     private val infoFile by option("-i", "--infoFile").file(exists = true).default(File("../info.json"))
     private val workspace by option("-w", "--workspace").file(exists = true)
     override fun run() {
